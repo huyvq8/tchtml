842a01f9b9ce727272a2a800a0f14eeb185c5ced
huyvq8 <huyvq8@users.noreply.github.com>
2026-01-22 10:43:14 +0700

    Update sidebar menu styling and template

--- js/sidebar-menu-template.js
+++ js/sidebar-menu-template.js
@@ -75,13 +75,6 @@ window.SidebarMenuTemplate = `
     </svg>
     <span>Settings</span>
 </a>
-<a href="#" class="nav-item" data-menu-item="help">
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <path d="M3 18v-6a9 9 0 0 1 18 0v6"></path>
-        <path d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3zM3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z"></path>
-    </svg>
-    <span>Help Center</span>
-</a>
 <a href="logout.html" class="nav-item" data-menu-item="logout">
     <svg width="20" height="20" viewBox="0 0 20 20" fill="none">
         <path d="M7 17L2 12M2 12L7 7M2 12H18" stroke="white" stroke-width="2" stroke-linecap="round"/>
